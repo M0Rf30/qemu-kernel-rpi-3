860fd1f48897e891f8d432d96a8b148174d0b7a7
M0Rf30 <morf3089@gmail.com>
2020-01-12 17:35:30 +0100

    Add model name to build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Build latest stable ARM kernel for QEMU Raspberry Pi 3 Emulation
 #
 #######################################################
+MODEL=rpi3
 TOOLCHAIN=aarch64-linux-gnu
 COMMIT=$(curl -s https://www.kernel.org | grep -A1 latest_link | tail -n1 | egrep -o '>[^<]+' | egrep -o '[^>]+')
 export ARCH=arm64
@@ -18,7 +19,7 @@ fi
 cd linux-$COMMIT
 
 KERNEL_VERSION=$(make kernelversion)
-KERNEL_TARGET_FILE_NAME=qemu-kernel-$KERNEL_VERSION-aarch64
+KERNEL_TARGET_FILE_NAME=qemu-kernel-$MODEL-$KERNEL_VERSION-aarch64
 echo "Building Qemu Raspberry Pi kernel qemu-kernel-$KERNEL_VERSION"
 
 # Config
